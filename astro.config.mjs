dfadbd7877ae5d18b386b9beddd93990cc6b58f7
notSagyo <notsagyo@gmail.com>
2025-03-02 01:23:20 -0300

    Bump packages to latest version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 import tunnel from 'astro-tunnel';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
   integrations: [tailwind(), react(), tunnel()],
+  output: 'static',
   adapter: vercel(),
 });
